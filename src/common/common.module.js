b9c4537dcaa171f13e7a57edfdd79298a5bd3adb
vinayakayr <44550020+vinayakayr@users.noreply.github.com>
2018-11-18 16:57:43 +0530

    Upadtes

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://vinayrpslv-course5.herokuapp.com')
+.constant('ApiPath', 'https://aleksandar-gjorgievski.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
